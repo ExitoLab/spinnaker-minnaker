1ab2249b2165ef45406146d6dadae1b6fb6b40da
Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>
2021-05-01 23:35:01 +0400

    feat: created infra in terraform for ubuntu
    Signed-off-by: Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>

---
+++ main.tf
@@ -0,0 +1,58 @@
+#create key pair on aws
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh_key"
+  public_key = var.public_key
+  tags       = local.tags
+}
+
+# vpc
+resource "aws_vpc" "vpc_1" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+}
+
+#subnet 
+resource "aws_subnet" "subnet_1" {
+  cidr_block              = var.subnet_cidr_1
+  vpc_id                  = aws_vpc.vpc_1.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+}
+
+#internet_gateway
+resource "aws_internet_gateway" "gateway_1" {
+  vpc_id = aws_vpc.vpc_1.id
+}
+
+#route_table
+resource "aws_route_table" "route_table_1" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway_1.id
+  }
+}
+
+resource "aws_route_table_association" "route_subnet_1" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.route_table_1.id
+}
+
+# ec2-instance
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet_1.id
+  vpc_security_group_ids = [aws_security_group.sg-instance.id]
+  key_name               = var.ssh_key_name
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+  }
+
+  tags       = local.tags
+}
---
+++ providers.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    aws = ">= 3.18"
+  }
+
+  backend "s3" {
+    bucket         = "terraform-practice"
+    key            = "spinnaker-minnaker/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform_practice_lock"
+
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
